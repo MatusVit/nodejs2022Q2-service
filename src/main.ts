8f1d937b4898399f9eb281372f3fa2079ba854fd
Matusevich Vitaliy <vimatu@gmail.com>
2022-07-14 20:46:14 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { dirname, join } from 'path';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const rootDirname = dirname(__dirname);
+  const docAPI = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = load(docAPI);
+  // console.log('docAPI >>> ', docAPI);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(port, () => console.log(`Server started on port ${port}`));
 }
 bootstrap();
